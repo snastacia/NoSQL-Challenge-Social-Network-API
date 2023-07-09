06e52711f5be6a8d2af6241644798b7e56a97cc6
Nastacia Shershova <znastacia@gmail.com>
2023-07-09 04:25:23 -0400

    final commit

---
+++ Develop/models/reaction.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const { Schema, Types } = mongoose;
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => createdAtVal.toLocaleString(),
+
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// const Reaction = model('Reaction', reactionSchema);
+
+module.exports = reactionSchema;
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const db = require('./Develop/config/connection');
+const routes = require('./Develop/routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}!`);
+  });
+});
